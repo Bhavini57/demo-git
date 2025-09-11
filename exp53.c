5b29ee0f344dd7f1085cf06095fd6cb72bf0e295
bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-09-12 00:53:34 +0530

    exp53.c

--- exp53.c
+++ exp53.c
@@ -2,16 +2,17 @@
 
 int main() {
     int year;
-    double population = 100000;  // starting population
+    double population = 100000;  
 
     printf("Initial Population = %.0f\n\n", population);
     printf("Population growth in last 10 years:\n\n");
 
     for (year = 1; year <= 10; year++) {
-        population = population + (population * 0.10); // increase by 10%
+        population = population + (population * 0.10); 
         printf("Year %d : %.0f\n", year, population);
     }
 
     return 0;
 }
+
 
