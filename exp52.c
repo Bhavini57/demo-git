963819032d7f6bcd303f0c5d07c8d37cfd846bd1
bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-09-12 00:21:25 +0530

    exp52.c

--- exp52.c
+++ exp52.c
@@ -3,18 +3,19 @@
 int main() {
     int num, i;
 
-    // asking for the number
+
     printf("Enter the number : ");
     scanf("%d", &num);
 
-    printf("\nthe multiplication table of %d:\n", num);
+    printf("the multiplication table of %d:\n", num);
 
-    // loop from 1 to 10
+    // rec till 10
     for (i = 1; i <= 10; i++) {
         printf("%d * %d = %d\n", num, i, num * i);
     }
 
     return 0;
 }
+
 
 
