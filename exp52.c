c9258257f5077a972478c3efc42756688cc64f98
bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-09-12 01:11:01 +0530

    Update exp52.c

--- exp52.c
+++ exp52.c
@@ -11,12 +11,14 @@ int main()
     printf("the multiplication table of %d:\n", num);
 
     // rec till 10
-    for (i = 1; i <= 10; i++) {
+    for (i = 1; i <= 10; i++)
+        {
         printf("%d * %d = %d\n", num, i, num * i);
     }
 
     return 0;
 }
+
 
 
 
