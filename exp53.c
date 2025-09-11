9a23818358ef7f98b6a8b5c15a3f406e8eeabc06
bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-09-12 01:08:16 +0530

    Update exp53.c

--- exp53.c
+++ exp53.c
@@ -4,15 +4,17 @@ int main() {
     int year;
     double population = 100000;  
 
-    printf("Initial Population = %.0f\n\n", population);
-    printf("Population growth in last 10 years:\n\n");
+    printf("Initial Population = %.0f\n", population);
+    printf("growth in last 10 years:\n");
 
-    for (year = 1; year <= 10; year++) {
+    for (year = 1; year <= 10; year++)
+        {
         population = population + (population * 0.10); 
         printf("Year %d : %.0f\n", year, population);
     }
 
     return 0;
 }
+
 
 
